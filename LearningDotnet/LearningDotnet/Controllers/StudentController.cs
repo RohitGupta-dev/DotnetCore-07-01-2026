0cd19ad5cb7e1a2eb111885f8e300a180c58b0cc
RohitGupta <rohit.revclerx@gmail.com>
2026-01-07 07:37:01 +0530

    my simple test api

---
+++ LearningDotnet/LearningDotnet/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using LearningDotnet.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningDotnet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("All",Name="GetAllStudent")]
+        public IEnumerable<Student> GetStudents()
+        {
+            return CollegeRepository.Students;
+        }
+        //[HttpGet("{id:int}")]
+        [HttpGet]
+        [Route("{id:int}", Name ="GetStudentById")]
+        public Student GetStudent(int id) {
+            return CollegeRepository.Students.Where(x => x.Id == id).FirstOrDefault();
+        }
+        //[HttpGet("{name:alpha}")]
+        [HttpGet()]
+        [Route("{name:alpha}" ,Name ="GetStudentByName")]
+        public Student GetStudent(string name) {
+            return CollegeRepository.Students.Where(x => x.StudentName == name).FirstOrDefault();
+        }
+        ////[HttpGet("{id:int}")]
+        ////[Route("{id:int}",Name = "DeleteStudentById")]
+        [HttpDelete()]
+        [Route("{id:int}", Name = "DeleteStudentById")]
+        public bool DeleteStudent(int id)
+        {
+            var student = CollegeRepository.Students.Where(x => x.Id == id).FirstOrDefault();
+            CollegeRepository.Students.Remove(student);
+            return true;
+        }
+    }
+}
---
+++ LearningDotnet/LearningDotnet/Models/CollegeRepository.cs
@@ -0,0 +1,29 @@
+﻿namespace LearningDotnet.Models
+{
+    public static class CollegeRepository
+    {
+        public static List<Student> Students = new List<Student>()
+        {
+
+            new Student()
+            {
+                Id=1,
+                StudentName="Rohit",
+                Email="test@gmail.com",
+                Address="#22 Rajpura",
+            },new Student()
+            {
+                Id=2,
+                StudentName="Ankit",
+                Email="test1@gmail.com",
+                Address="#22 Mohali",
+            },new Student()
+            {
+                Id=3,
+                StudentName="Mohit",
+                Email="test3@gmail.com",
+                Address="#22 Chandigadh",
+            }
+        };
+    }
+}
